6ea3e011828d889e6d7254cd5648f98ea441c51c
Vitali060107 <00001080660343sp@al.educacao.sp.gov.br>
2024-11-07 10:31:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Brigerton',
+   'Quem é o diamante da primeira temporada?',
+    'Resposta:Daphine!'
+)
+criaCartao(
+    'temporada 2',
+    'com quem anthony pedi em casamento primeiro?',
+    'Resposta:Edwina'
+)
+criaCartao(
+    'temporada 3',
+    'Quem é a lady whistledown jornal?',
+    'Resposta:Penelope  featherington'
+)
+criaCartao(
+    'aleatoria',
+    'como o visconde brigerton morreu ?',
+    'picada de abelha'
+)
